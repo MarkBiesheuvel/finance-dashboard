b2a3422d70047aa5c5cb900bbcfb7ecb8a427dd9
Mark Biesheuvel <mail@markbiesheuvel.nl>
2021-02-28 20:04:37 +0100

    Improve error handling of query function

--- src/query/query.py
+++ src/query/query.py
@@ -47,11 +47,25 @@ def get_records_by_daterange(ticker, start, end):
     return response['Items']
 
 
+@xray_recorder.capture('get_dates_from_event')
+def get_dates_from_event(event):
+    if 'queryStringParameters' not in event:
+        return None, None
+
+    qs = event['queryStringParameters']
+
+    if qs is None or \
+            'start' not in qs or \
+            'end' not in qs:
+        return None, None
+
+    return qs['start'], qs['end']
+
+
 @xray_recorder.capture('handler')
 def handler(event, context):
-    ticker = event.get('pathParameters', {}).get('ticker', '')
-    start = event.get('queryStringParameters', {}).get('start', '')
-    end = event.get('queryStringParameters', {}).get('end', '')
+    ticker = event.get('pathParameters', {}).get('ticker', 'AMZN')
+    start, end = get_dates_from_event(event)
 
     if start and end:
         items = get_records_by_daterange(ticker, start, end)
